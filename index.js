e68015542bcafb83672ca257f5101c6035913861
abdul rehman e <abdulrkhan570@gmail.com>
2023-02-16 19:10:18 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 let arr = document.getElementsByClassName('drum');
 Array.from(arr).forEach((elm, index) => {
     elm.addEventListener('click', () => {
+        elm.classList.add("pressed");
         let audio = new Audio(`sounds/${index + 1}.mp3`);
         audio.play();
+       setInterval(() => {
+            elm.classList.remove("pressed");
+       }, 200); 
+        
     })
     document.addEventListener('keydown', (event) => {// keyboard event listner will be used on document 
-        let key = event.key;
+        let key = event.key;// event.key will give the String of which key is preseed;
         let audio;
+        console.log(key);
+        document.getElementsByClassName(key)[0].classList.add("pressed");
+        setInterval(() => {
+            document.getElementsByClassName(key)[0].classList.remove("pressed");
+        }, 200);
         switch (key) {
             case "w":
                 audio = new Audio("sounds/1.mp3")
